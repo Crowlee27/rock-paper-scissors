6cffa0e6b244a79dc3bb581392a0a10669b8f479
Cory <rowlee.development@gmail.com>
2022-08-02 03:48:20 -0500

    add problem steps

--- javascript.js
+++ javascript.js
@@ -1,41 +1,48 @@
+/*create rock paper scissors
+steps of problem solving
+ask if you want to play rock paper scissors
+make the choices for rock paper scissors
+ask the player to pick rock paper or scissors
+randomize rock paper scissors for the computer
+explain which hand beats what
+player won/player lost
+ask if you want to play agin
+reset game
+run again
+*/ 
+
+
+
+
+
+
 function getComputerChoice() {
-   const randomHand = (Math.floor(Math.random()*3))
-   if (randomHand === 0) {
+  const randomHand = Math.floor(Math.random() * 3);
+  if (randomHand === 0) {
     return "Rock";
-   }
-   else if (randomHand === 1) {
+  } else if (randomHand === 1) {
     return "Paper";
-   }
-   else if (randomHand === 2) {
+  } else if (randomHand === 2) {
     return "Scissor";
-   }
+  }
 }
 
-
 function computerSelection() {
-     return getComputerChoice();
+  return getComputerChoice();
 }
 
 function playerSelection() {
-    let myHand = prompt("Please pick a hand");
-    let myHand2 = myHand.charAt(0).toUpperCase() + myHand.slice(1).toLowerCase();
-    if (myHand2 === "Rock") {
-        return "Rock";
-    }
-    else if (myHand2 === "Paper") {
-        return "Paper";
-    }
-    else if (myHand2 === "Scissor") {
-        return "Scissor";
-    }
-
+  let myHand = prompt("Please pick a hand");
+  let myHand2 = myHand.charAt(0).toUpperCase() + myHand.slice(1).toLowerCase();
+  if (myHand2 === "Rock") {
+    return "Rock";
+  } else if (myHand2 === "Paper") {
+    return "Paper";
+  } else if (myHand2 === "Scissor") {
+    return "Scissor";
+  }
 }
 
-function singleGame() {
+function singleGame() {}
 
-}
-
-
-
-
-console.log(playerSelection())
+console.log(playerSelection());
