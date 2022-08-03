dde10591c8f28b0ac563cef9150454cea72a96de
Cory <rowlee.development@gmail.com>
2022-08-03 00:06:50 -0500

    add/fix playRound bug

--- javascript.js
+++ javascript.js
@@ -23,14 +23,14 @@ function getComputerChoice() {
   } else if (computerHand === 2) {
     return "Paper";
   } else if (computerHand === 3) {
-    return "Scissor";
+    return "Scissors";
   }
 }
 
 const computerSelection = getComputerChoice()
 
 
-function playerSelection() {
+function playerSelectionChoice() {
   const playerChoice = prompt("Please pick a hand");
   const playerHand = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
     if (playerHand === "Rock") {
@@ -44,32 +44,54 @@ function playerSelection() {
     }
 }
 
+const playerSelection = playerSelectionChoice()
+
+/*
+was given as a pointer to store the return values as a varible. writting down for notes.
 
 function playRound() {
-  if (playerSelection() === computerSelection) {
+  let pSelect = playerSelection(); //store to variable
+  let cSelect = computerSelection(); //store to variable
+  
+  if (pSelect == cSelect) { // easier to read when comparing values
+    ..
+  } */
+
+
+
+function playRound() {
+  if (playerSelection === computerSelection) {
     return "It's a tie!";
   }
-  else if (playerSelection() === "Rock" && computerSelection === "Paper") {
+  else if (playerSelection === "Rock" && computerSelection === "Paper") {
     return "You lose, Paper beats Rock!";
   }
-  else if (playerSelection() === "Rock" && computerSelection === "Scissors") {
-    return "You win, Rock beats Scissors";
+  else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    return "You win, Rock beats Scissors!";
   }
-  else if (playerSelection() === "Paper" && computerSelection === "Scissors") {
+  else if (playerSelection === "Paper" && computerSelection === "Scissors") {
     return "You lose, Scissors beats Paper!";
   }
-  else if (playerSelection() === "Paper" && computerSelection === "Rock") {
-    return "You win, Paper beats Rock";
+  else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return "You win, Paper beats Rock!";
   }
-  else if (playerSelection() === "Scissors" && computerSelection === "Rock") {
+  else if (playerSelection === "Scissors" && computerSelection === "Rock") {
     return "You lose, Rock beats Scissors!";
   }
-  else if (playerSelection() === "Scissors" && computerSelection === "Paper") {
-    return "You win, Scissors beats Paper";
+  else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    return "You win, Scissors beats Paper!";
   }
   else {
     return "something went wrong"
   }
 }
 
+const computerScore = 0;
+const playerScore = 0;
 
+function game(playerSelection, computerSelection) {
+}
+
+console.log(playerSelection)
+console.log(computerSelection)
+console.log(playRound())
